1d247854584d2d7060fa454ed63c3ef627c537e0
samuel almeida <samuel.ralmeida@gmail.com>
2021-01-08 20:58:09 -0300

    created usecases skeleton

---
+++ pkg/usecases/nota.go
@@ -0,0 +1,30 @@
+package usecases
+
+import (
+	"apps/investimento/pkg/model"
+	"context"
+)
+
+type notaUsecase struct {
+	notaRepository model.NotaRepository
+}
+
+func NewNotaUsecase(repo model.NotaRepository) model.NotaUsecase {
+	return &notaUsecase{
+		notaRepository: repo,
+	}
+}
+
+func (n *notaUsecase) Fetch(ctx context.Context) ([]model.Nota, error) {
+	panic("ahhhh")
+}
+
+func (n *notaUsecase) GetByID(ctx context.Context, id int) (model.Nota, error) {
+	panic("ahhhh")
+}
+func (n *notaUsecase) Save(ctx context.Context, nota *model.Nota) error {
+	panic("ahhhh")
+}
+func (n *notaUsecase) Delete(ctx context.Context, nota *model.Nota) error {
+	panic("ahhhh")
+}
