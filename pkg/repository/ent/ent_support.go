84cbd2b11560377cfacb1eef455afda2faf57481
samuel almeida <samuel.ralmeida@gmail.com>
2021-01-19 19:25:32 -0300

    fixed error handler

--- pkg/repository/ent/ent_support.go
+++ pkg/repository/ent/ent_support.go
@@ -5,13 +5,13 @@ import (
 
 	"apps/investimento/pkg/ent"
 
-	"github.com/pkg/errors"
+	"apps/investimento/pkg/support/errors"
 )
 
 func WithTx(ctx context.Context, client *ent.Client, fn func(tx *ent.Tx) error) error {
 	tx, err := client.Tx(ctx)
 	if err != nil {
-		return err
+		return errors.Wrap("ent:support:withtx:client", err)
 	}
 	defer func() {
 		if v := recover(); v != nil {
@@ -21,12 +21,13 @@ func WithTx(ctx context.Context, client *ent.Client, fn func(tx *ent.Tx) error) 
 	}()
 	if err := fn(tx); err != nil {
 		if rerr := tx.Rollback(); rerr != nil {
-			err = errors.Wrapf(err, "rolling back transaction: %v", rerr)
+			e := errors.Wrap("ent:support:withtx:rollback", rerr)
+			err = errors.Wrap(e.Error(), err)
 		}
 		return err
 	}
 	if err := tx.Commit(); err != nil {
-		return errors.Wrapf(err, "committing transaction: %v", err)
+		return errors.Wrap("ent:support:withtx:commit", err)
 	}
 	return nil
 }
